9ee2c627b980a6952e21ca1e15ff98e51886064b
talisker18 <hjoel87@gmx.ch>
2022-05-28 10:35:01 +0200

    story 4.1 application skeleton

---
+++ src/main/java/com/henz/MediCareApplication.java
@@ -0,0 +1,13 @@
+package com.henz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediCareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/henz/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.henz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value={"","/"}, method = RequestMethod.GET)
+	public ModelAndView showHomePage() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value={"support"}, method = RequestMethod.GET)
+	public ModelAndView showSupportPage() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("profile-dropdown/support");
+		
+		return mv;
+	}
+
+}
